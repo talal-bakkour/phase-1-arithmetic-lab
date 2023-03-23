7eca2d3fb1db1ce4f8990069f7ef08fe7b15fdd9
talal <ta440237@gmail.com>
2023-03-23 19:01:57 +0300

    Done with assignment arithmtic lap

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3=10;
+let num4=24
+let mod = num4 % num3;
 
-// Write your code here
+let max = Math.max(1, 2, 3, 4, 20);
+// Write your code her
